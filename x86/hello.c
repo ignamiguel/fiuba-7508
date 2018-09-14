306cf9b1bfee17587e37a842dd8e219ce2251eef
ignamiguel <iglesiasnacho@gmail.com>
2018-09-14 07:06:07 -0300

    Working on tp2

--- x86/hello.c
+++ x86/hello.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
+#include <stdio.h>
 
 const char msg[] = "Hello, world!\n";
+// const char *msg = "Hello, world!\n";
+const char letter = 'A';
+
 
 int main(void) {
+    // printf("Hola\n");
+    // printf("%c \n", letter);
+    // printf("%d \n", sizeof letter);
+    // printf("%d \n", sizeof msg);
     write(1, msg, sizeof msg - 1);
     _exit(7);
 }
